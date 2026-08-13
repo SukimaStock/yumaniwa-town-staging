686b1a4dff588b5a197327752909ae2bd2edf92e
SukimaStock <tavchegravche.sk@gmail.com>
2026-08-13 21:06:08 +0200

    Update town-runtime-fixes.js

--- data/town-runtime-fixes.js
+++ data/town-runtime-fixes.js
@@ -136,7 +136,7 @@
       upsertProp(onsen, {
         id: 'no_entry_sign',
         src: 'assets/maps/props/common/no-entry-sign.png',
-        x: 10, y: 2, w: 4, h: 4, footY: 6, enabled: true,
+        x: 10, y: 2, w: 1, h: 1, footY: 6, enabled: true,
         collision: { enabled: false, x: 0, y: 0, w: 0, h: 0 },
         interaction: { enabled: false, triggerId: '', x: 0, y: 0, w: 0, h: 0 },
         catalogKey: 'bench'
