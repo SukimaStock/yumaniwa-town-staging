215b1ba8652e41110d2badca3c9ee518d29a2bfc
SukimaStock <262211877+SukimaStock@users.noreply.github.com>
2026-09-22 14:51:25 +0200

    Add CoffeeFactory SE config for Sound Lab

---
+++ works/coffee-factory/coffee-sound-config.js
@@ -0,0 +1,12 @@
+// Canonical CoffeeFactory sound-effect configuration.
+// CoffeeFactory Sound Lab can replace this file together with approved SE files.
+window.COFFEEFACTORY_SOUND_CONFIG = Object.freeze({
+  ui_select: Object.freeze({ file: "./assets/audio/ui_select.wav", mode: "buffer", volume: 0.34, cooldown: 45 }),
+  ui_step: Object.freeze({ file: "./assets/audio/ui_step.wav", mode: "buffer", volume: 0.32, cooldown: 35 }),
+  factory_start: Object.freeze({ file: "./assets/audio/factory_start.wav", mode: "buffer", volume: 0.42, cooldown: 160 }),
+  grinder_lever: Object.freeze({ file: "./assets/audio/grinder_lever.wav", mode: "buffer", volume: 0.38, cooldown: 1100 }),
+  steam_soft: Object.freeze({ file: "./assets/audio/steam_soft.wav", mode: "buffer", volume: 0.22, cooldown: 2400 }),
+  brew_ready: Object.freeze({ file: "./assets/audio/brew_ready.wav", mode: "buffer", volume: 0.48, cooldown: 60 }),
+  brew_change: Object.freeze({ file: "./assets/audio/brew_change.wav", mode: "buffer", volume: 0.62, cooldown: 100 }),
+  brew_finish: Object.freeze({ file: "./assets/audio/brew_finish.wav", mode: "buffer", volume: 0.68, cooldown: 200 }),
+});
